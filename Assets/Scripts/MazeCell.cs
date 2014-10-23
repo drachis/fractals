180faae173888448d8c5a302bfdeb86d70f97f6e
Toli <drachis+git@gmail.com>
2014-10-23 00:57:00 -0700

    maze in generating linearly, and has a custom IntVector2 to make life easy

---
+++ Assets/Scripts/MazeCell.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeCell : MonoBehaviour {
+
+    public int sizeX, sizeZ;
+    
+    public MazeCell cellPrefab;
+
+    public IntVector2 coordinates;
+    
+    private MazeCell[,] cells;   
+}
